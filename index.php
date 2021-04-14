9c3f4e9bf53fc79720c1bf70f7bf0d079388d4ef
Arpan pradhan <59037548+Newar1@users.noreply.github.com>
2021-04-14 13:00:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Download previoues question</title>
+
+
+</head>
+<body>
+
+    <center>
+            <h2 style="color: #d7385e">Welcome to Computer science department</h2>
+            <p><b>click here to Download</b></p>
+
+            <a href="Computer_Science_CBCS_Final_(1)-1.pdf" target="newar">Download pdf now</a>
+            
+        
+    </center>
+    
+</body>
+</html>
